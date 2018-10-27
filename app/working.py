817b10c7e8208f347069999293868b4b9730cbb7
umangahuja1 <umangahuja1@gmail.com>
2018-10-27 06:11:17 +0530

    working app added

---
+++ app/working.py
@@ -0,0 +1,77 @@
+import boto3
+import cv2
+import os
+import shutil
+
+bucket='shortify1'
+
+dir = 'b/'
+video = 'test_video2.mkv'
+video_link = 'https://www.youtube.com/watch?v=WANWx2C8Jak'
+
+if os.path.exists(dir):
+    shutil.rmtree(dir)
+os.makedirs(dir)
+
+client = boto3.client('rekognition')
+
+cap = cv2.VideoCapture(video_link)
+i = 1000
+m = 1
+ret = True
+
+short_frames = []
+
+while(ret and cap.isOpened()):
+    try:
+        file = dir + str(m) + '.jpg'
+        ret, frame = cap.read()
+        cap.set(1, i*m )
+        m+=1
+
+        cv2.imwrite(file, frame)
+        short_frames.append(frame)
+    except:
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+print(m)
+
+for x in range(1,m):
+    try:
+        file = dir + str(x) + '.jpg'
+        with open(file, 'rb') as image:
+            response = client.detect_text(Image={'Bytes': image.read()})
+
+        textDetections = response['TextDetections']
+
+        for text in textDetections:
+            print(text['DetectedText'])
+    except:
+        pass
+
+image_folder = dir
+video_name = 'output.avi'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+images.sort(key = lambda x: int(x.split('.')[0]))
+
+print(images)
+
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (width, height))
+for image in images:
+    print(image)
+    img = cv2.imread(os.path.join(image_folder, image))
+    try:
+        out.write(img)
+    except:
+        pass
+
+cv2.destroyAllWindows()
